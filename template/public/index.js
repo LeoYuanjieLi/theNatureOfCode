2de57fd290c6fa9de83dcfc58831cbc0d115edb9
Leo Li <leo.li@adidas.com>
2018-12-22 22:48:42 -0800

    add back app.js in projectOne

--- template/public/index.js
+++ template/public/index.js
@@ -1,12 +1,3 @@
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  };
