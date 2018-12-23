572455eec648da0eba111c8daa107354b98a5cc3
Leo Li <leo.li@adidas.com>
2018-12-22 22:55:49 -0800

    fixed small bugs

--- template/public/index.js
+++ template/public/index.js
@@ -1,3 +1,12 @@
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+    createCanvas(window.innerWidth, window.innerHeight);
   };
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+};
